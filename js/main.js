f91868183fc90d7a88ecae5c1ed1e6431042ecf5
Oskar Sjöberg <oskar@oskarsjoberg.se>
2016-09-09 14:00:30 +0200

    Tog bort smoothscroll.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
 jQuery(function($) {
     'use strict';
 
-    // Navigation Scroll
-    $(window)
-        .scroll(function(event) {
-            Scroll();
-        });
-
     $('.navbar-collapse ul li a')
         .on('click',
             function() {
@@ -20,30 +14,6 @@ jQuery(function($) {
                 $('html, body').animate({ scrollTop: $(this.hash).offset().top - 5 }, 1000);
                 return false;
             });
-
-    // User define function
-    function Scroll() {
-        var contentTop = [];
-        var contentBottom = [];
-        var winTop = $(window).scrollTop();
-        var rangeTop = 200;
-        var rangeBottom = 500;
-        $('.navbar-collapse')
-            .find('.scroll a')
-            .each(function() {
-                contentTop.push($($(this).attr('href')).offset().top);
-                contentBottom.push($($(this).attr('href')).offset().top + $($(this).attr('href')).height());
-            })
-        $.each(contentTop,
-            function(i) {
-                if (winTop > contentTop[i] - rangeTop) {
-                    $('.navbar-collapse li.scroll')
-                        .removeClass('active')
-                        .eq(i)
-                        .addClass('active');
-                }
-            })
-    };
 
     $('#tohash')
         .on('click',
@@ -157,8 +127,6 @@ jQuery(function($) {
 
     //Initiat WOW JS
     new WOW().init();
-    //smoothScroll
-    smoothScroll.init();
 
     // portfolio filter
     $(window)
